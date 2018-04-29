bf8b7bee83415bed54af070fd72a028cc125696b
Alberto <albon_marvel@hotmail.com>
2018-04-29 00:49:41 -0500

    agregando componente de registro

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { User } from '../../modelos/user';
+import { UserService } from '../../services/user/user.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  providers: [UserService]
 })
 export class RegisterComponent implements OnInit {
+  public user: User;
+  private status: string;
+  private mensaje: string;
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _userService: UserService
+  ) {
+      this.user = new User("", "", "", "", "");
+   }
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onSubmit(registerForm){
+    this._userService.register(this.user)
+    .subscribe(
+      response => {
+          if(response.user && response.user._id){
+            this.status = "success";
+            this.mensaje = "Registro exitoso!";
+            registerForm.reset();
+          } else {
+            this.status = "warning";
+            this.mensaje = response.message;
+          }
+      }, error => {
+          console.log(<any>error);
+      }
+    );
+    //console.log(this.UserService);
   }
 
 }
