80e58b43d0088b7f2b6419a2b092c2aad691f398
Alberto <albon_marvel@hotmail.com>
2018-04-29 18:10:01 -0500

    creando servicio de usuarios

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,18 +1,48 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Obserbable } from 'rxjs/Obserbable';
+import { Observable } from 'rxjs/Observable';
 import { GLOBAL } from '../global/global.service';
 import { User } from '../../modelos/user';
 
 @Injectable()
 export class UserService {
   public url:string;
+  private headers: HttpHeaders;
   constructor(public _http: HttpClient) {
     this.url = GLOBAL.url;
+    this.headers = new HttpHeaders().set('Content-Type', 'application/json');
   }
-  register(user: User): Obserbable<any>{
+  register(user: User): Observable<any>{
+    return this.postUser('registrar', user);
+  }
+
+  signUp(user:User, token = null): Observable<any> {
+    if(token != null){
+      user.token = token;
+    }
+    return this.postUser('login', user);
+  }
+
+  postUser(ruta: string, user){
     let params = JSON.stringify(user);
-    let headers = new HttpHeaders().set('Content-Type', 'application/json');
-    return this._http.post(this.url + 'registrar', params, { headers: headers });
+    return this._http.post(this.url + ruta, params, { headers: this.headers });
+  }
+
+  getIdentity(){
+    let identity = JSON.parse(localStorage.getItem('identity'));
+    if(identity != 'undefined'){
+      return identity;
+    } else {
+      return false;
+    }
+  }
+
+  getToken() {
+    let token = localStorage.getItem('token');
+    if(token != 'undefined'){
+      return token;
+    } else {
+      return false;
+    }
   }
 }
