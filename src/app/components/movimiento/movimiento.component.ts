401efcc6745ad83f054f5904c250236c65f7304a
Alberto Bonifacio Martinez Velasco <alberto.martinezve@famsa.com>
2020-02-25 17:18:02 -0600

    permitir guardar multiples movimientos

--- src/app/components/movimiento/movimiento.component.ts
+++ src/app/components/movimiento/movimiento.component.ts
@@ -6,6 +6,8 @@ import { Concepto } from '../../modelos/concepto';
 import { ConceptoService } from '../../services/concepto/concepto.service';
 import { CategoriaService } from '../../services/categoria/categoria.service';
 import { MovimientoService } from '../../services/movimiento/movimiento.service';
+
+import {forkJoin} from 'rxjs';
 @Component({
   selector: 'app-movimiento',
   templateUrl: './movimiento.component.html',
@@ -17,6 +19,13 @@ export class MovimientoComponent implements OnInit {
   public categoriaSelected: Categoria;
   public conceptoSelected: Concepto;
   public categorias: Array<Categoria>;
+
+  public total :number;
+
+  public conceptosSelected: Array<Concepto>;
+
+  public movimientos: Array<Movimiento>;
+
   public conceptos: Array<Concepto>;
   constructor(
     private _categoriaService: CategoriaService,
@@ -25,6 +34,10 @@ export class MovimientoComponent implements OnInit {
     private toastr: ToastrService
   ) {
     this.categoriaSelected = null;
+    this.movimientos = [];
+    this.conceptosSelected = [];
+
+    this.total = 0;
   }
 
   ngOnInit() {
@@ -64,7 +77,19 @@ export class MovimientoComponent implements OnInit {
       let conceptselect = this.conceptoSelected;
       this.movimiento.concepto_id = conceptselect._id;
       //this.movimiento.fecha = new Date(this.movimiento.fecha);
+      let movimiento = Object.assign({} , this.movimiento);
+      this.movimientos.push( movimiento );
+      this.conceptosSelected.push( this.conceptoSelected );
+      this.total = 0;
+      this.movimientos.forEach(  ( movimiento ) => {
+        
+        this.total = movimiento.monto + this.total;
+        
+      });
       
+      var fecha = this.movimiento.fecha;
+      this.movimiento = new Movimiento("", 0, "", 0, "", fecha);
+      /*
       this._movimientoService.addMovimiento(this.movimiento)
       .subscribe(
           response => {
@@ -75,9 +100,32 @@ export class MovimientoComponent implements OnInit {
               console.log(error);
           }
       )
+      */
     } else {
         this.toastr.error('Alerta!', 'No se ha seleccionado ningun concepto');
     }
+  }
+
+  getNameConcepto(idConcepto){
+    let concepto = this.conceptosSelected.find( (concepto) => concepto._id == idConcepto );
+    return concepto;
+  }
+
+  guardar(){
+
+    let request = [];
+    this.movimientos.forEach( ( movimiento ) => {
+      request.push( this._movimientoService.addMovimiento(movimiento) );
+      
+    });
+
+    forkJoin( request )
+    .subscribe(allResults => {
+      
+      this.toastr.success('OK!', 'Agregados');
+      this.movimientos = [];
+      this.conceptosSelected = [];
+    });
   }
 
 }
